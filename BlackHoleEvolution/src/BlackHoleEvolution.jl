a2cdc968c06142e59d1b1135f9546c3bdb24362f
Quan-feng WU <32387646+Fenyutanchan@users.noreply.github.com>
2024-07-28 09:45:26 +0800

    using new utils in BlackHoleEvolution

--- BlackHoleEvolution/src/BlackHoleEvolution.jl
+++ BlackHoleEvolution/src/BlackHoleEvolution.jl
@@ -5,10 +5,10 @@ using NaturalUnits
 export BH_mass
 
 function BH_mass(M0::EnergyUnit, t::EnergyUnit; t0=MeV(0, -1), g::Real=1)
-    @assert EUdim(M0) == 1 "M0 should have mass dimension of 1, but got $(EUdim(M0))."
-    @assert EUdim(t) == -1 "t should have mass dimension of -1, but got $(EUdim(t))."
-    @assert M0 > zero(M0) "M0 should be positive, but got $(M0)."
-    @assert g > 0 "g should be positive, but got $(g)."
+    @check_EU_dimension M0 1
+    @check_EU_dimension t -1
+    @check_nonnegative_value M0
+    @check_positive_value g
 
     t < t0 && return M0
     t > t0 && return zero(M0)
@@ -16,9 +16,9 @@ function BH_mass(M0::EnergyUnit, t::EnergyUnit; t0=MeV(0, -1), g::Real=1)
 end
 
 function τ_BH(M0::EnergyUnit; g::Real=1)
-    @assert EUdim(M0) == 1 "M0 should have mass dimension of 1, but got $(EUdim(M0))."
-    @assert M0 > zero(M0) "M0 should be positive, but got $(M0)."
-    @assert g > 0 "g should be positive, but got $(g)."
+    @check_EU_dimension M0 1
+    @check_nonnegative_value M0
+    @check_positive_value g
 
     NU = (NaturalUnit ∘ typeof)(M0)
     M_Pl = NU.M_Pl
